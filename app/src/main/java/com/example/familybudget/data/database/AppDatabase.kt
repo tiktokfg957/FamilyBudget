dee186d34128d77f36d7d257f82415f0735ad3c2
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-07 04:42:58 +0700

    Update AppDatabase.kt

--- app/src/main/java/com/example/familybudget/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/familybudget/data/database/AppDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import com.example.familybudget.data.model.Category
 import com.example.familybudget.data.model.Transaction
 
@@ -13,7 +12,6 @@ import com.example.familybudget.data.model.Transaction
     version = 3,
     exportSchema = false
 )
-@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun transactionDao(): TransactionDao
     abstract fun categoryDao(): CategoryDao
